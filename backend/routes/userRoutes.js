14f1def2f447c13fea6b17b72dbada275ef725be
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-11-13 18:57:14 +0530

    Routes Added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const{register, login, getProfile} = require('../controllers/authController');
+const{register, login, getProfile,updateProfile, deleteProfile } = require('../controllers/authController');
 const  authMiddleware  = require('../middleware/authMiddleware');
 // User registration route
 router.post('/register', register);
@@ -8,4 +8,8 @@ router.post('/register', register);
 router.post('/login', login);
 // Get user profile route
 router.get('/profile', authMiddleware, getProfile);
+// Update user profile route
+router.put('/profile', authMiddleware, updateProfile);
+// Delete user profile route
+router.delete('/profile', authMiddleware, deleteProfile);
 module.exports = router;
