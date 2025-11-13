e292e980b619ff4fd5f547dc7e785453e13ab0cf
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-11-13 18:42:24 +0530

    URL ADDED

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const cors = require('cors');
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://user-bank-management.vercel.app",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:4000/api/v1';
+const API_BASE_URL = 'https://userbankmanagement.onrender.com/api/v1';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
